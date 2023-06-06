7437c80478a9b9d9ce7db44ee96111944b36cc65
Alexi Chepura <a.chepura@gmail.com>
2023-06-06 12:58:53 +0300

    fix site-run hanging

--- ops/site-run.sh
+++ ops/site-run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 set -o allexport; source .env; set +o allexport
 
-ssh $SOZU_HOST \
-"lsof -t -i:3000 | xargs -r kill && \
+ssh $SOZU_HOST "
+lsof -t -i:3000 | xargs -r kill
 DATABASE_URL=file:$SERVER_DIR/site.db \
 LEPTOS_SITE_ADDR=127.0.0.1:3000 \
 LEPTOS_SITE_ROOT=$SERVER_DIR/site \
 LEPTOS_OUTPUT_NAME=$NAME \
-nohup $SERVER_DIR/$NAME > $SERVER_DIR/nohup.out 2>&1 & disown"
+nohup $SERVER_DIR/$NAME >$SERVER_DIR/nohup.out 2>./nohup.err &
+"
