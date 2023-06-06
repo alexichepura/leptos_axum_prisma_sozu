474a1710159e448bd1ad319985c18712f9c139d0
Alexi Chepura <a.chepura@gmail.com>
2023-06-06 13:01:49 +0300

    fix nohup.err path

--- ops/site-run.sh
+++ ops/site-run.sh
@@ -7,5 +7,5 @@ DATABASE_URL=file:$SERVER_DIR/site.db \
 LEPTOS_SITE_ADDR=127.0.0.1:3000 \
 LEPTOS_SITE_ROOT=$SERVER_DIR/site \
 LEPTOS_OUTPUT_NAME=$NAME \
-nohup $SERVER_DIR/$NAME >$SERVER_DIR/nohup.out 2>./nohup.err &
+nohup $SERVER_DIR/$NAME >$SERVER_DIR/nohup.out 2>$SERVER_DIR/nohup.err &
 "
