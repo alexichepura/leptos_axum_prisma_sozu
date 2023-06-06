6f95958cad579ed02ecbd00db9fc4efac257782b
Alexi Chepura <a.chepura@gmail.com>
2023-06-06 11:02:55 +0300

    cert-bot added domain to script

--- ops/cert-bot.sh
+++ ops/cert-bot.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
+set -o allexport; source .env; set +o allexport
 
 certbot certonly \
   --manual \
   --preferred-challenges dns \
-  --work-dir ./ops/cert --logs-dir ./ops/cert --config-dir ./ops/cert
+  --work-dir ./ops/cert --logs-dir ./ops/cert --config-dir ./ops/cert \
+  -d $DOMAIN
