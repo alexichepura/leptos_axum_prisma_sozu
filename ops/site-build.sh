9d84034563c1b841ed4e84f760441729a3c6e837
Alexi Chepura <a.chepura@gmail.com>
2023-06-06 12:16:15 +0300

    cargo prisma db push

--- ops/site-build.sh
+++ ops/site-build.sh
@@ -2,6 +2,11 @@
 set -o allexport; source .env; set +o allexport
 
 rustup target add $TARGET
-cargo prisma generate
+if [ ! -f ./site.db ];
+then
+    cargo prisma db push
+else
+    cargo prisma generate
+fi
 LEPTOS_BIN_TARGET_TRIPLE=$TARGET cargo leptos build --release
 precompress --brotli --deflate --gzip --zstd target/site/pkg
